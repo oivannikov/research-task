9f0843bec5ca51f2c8f29591e414e8cb57d48086
oivannikov <alex0502ivannikov@gmail.com>
2020-11-16 21:20:24 +0200

    fix URL path

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Navbar } from './components/Navbar/Navbar';
 import { TodosPage } from './pages/TodosPage/TodosPage';
 
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import { Route, BrowserRouter, Switch, Redirect } from 'react-router-dom';
 
 import { AboutPage } from './pages/AboutPage/AboutPage';
 
