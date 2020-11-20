4b479776be9956fb145a411caf61526d796ac518
oivannikov <alex0502ivannikov@gmail.com>
2020-11-21 00:54:05 +0200

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import './App';
+import './App.scss';
 
 const App: React.FC = () => {
   
   return (
     <div className="app">
-      
+      <h1>Hello Typescript</h1>
     </div>
   );
 }
